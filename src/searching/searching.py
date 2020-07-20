ddbf2dec6419c3f0f456af21323fda8495451ad9
Tim Shaker <tim@tmshkr.com>
2020-07-20 13:20:47 -0700

    complete binary_search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,3 +1,4 @@
+# Runtime complexity: O(n)
 def linear_search(arr, target):
     index = 0
     while index < len(arr):
@@ -8,9 +9,18 @@ def linear_search(arr, target):
     return -1   # not found
 
 
-# Write an iterative implementation of Binary Search
+# Runtime complexity: O(log n)
 def binary_search(arr, target):
+    left = 0
+    right = len(arr)-1
 
-    # Your code here
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif target < arr[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
 
     return -1  # not found
